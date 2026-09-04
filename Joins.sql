fac9b15c842c13f3bbab0dc38a8b2020e3d137fc
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-09-04 12:28:18 +0530

    Add FULL OUTER JOIN example in Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -6,3 +6,8 @@ select d.dep_name,avg(s.cgpa) as avg_cgpa from student s inner join department d
 
 # LEFT JOIN
 select s.name,e.course_id,e.grade from student s left join enrollment e on s.roll_no=e.roll_no;
+
+# FULL OUTTER JOIN
+select s.name,e.course_id,e.grade from student s left join enrollment e on s.roll_no=e.roll_no 
+union 
+select s.name,e.course_id,e.grade from student s right join enrollment e on s.roll_no=e.roll_no;
