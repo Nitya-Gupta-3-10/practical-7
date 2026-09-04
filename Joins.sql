08f9068a20a46a60a0ac8843cda49ce0c0d6f63d
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-09-04 12:09:21 +0530

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -3,3 +3,6 @@ select s.name,s.cgpa,d.dep_name from student s inner join department d on s.dep_
 select s.name,e.course_id,e.grade from student s inner join enrollment e on s.roll_no=e.roll_no;
  select d.dep_name,count(*) as student_count from student s inner join department d on s.dep_id=d.dep_id group by d.dep_name;
 select d.dep_name,avg(s.cgpa) as avg_cgpa from student s inner join department d on s.dep_id=d.dep_id group by d.dep_name;
+
+# LEFT JOIN
+select s.name,e.course_id,e.grade from student s left join enrollment e on s.roll_no=e.roll_no;
