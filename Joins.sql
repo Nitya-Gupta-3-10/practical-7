ff202b16b421ea9ffe4548a63eaf23621ebb2810
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-09-04 11:58:56 +0530

    Add query to count students by department

--- Joins.sql
+++ Joins.sql
@@ -1,3 +1,4 @@
 # INNER JOIN
 select s.name,s.cgpa,d.dep_name from student s inner join department d on s.dep_id=d.dep_id;
 select s.name,e.course_id,e.grade from student s inner join enrollment e on s.roll_no=e.roll_no;
+ select d.dep_name,count(*) as student_count from student s inner join department d on s.dep_id=d.dep_id group by d.dep_name;
