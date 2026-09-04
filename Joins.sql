de2084950944767fb5771767b37dfd08caa0334a
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-09-04 12:00:33 +0530

    Add query to calculate average CGPA by department

--- Joins.sql
+++ Joins.sql
@@ -2,3 +2,4 @@
 select s.name,s.cgpa,d.dep_name from student s inner join department d on s.dep_id=d.dep_id;
 select s.name,e.course_id,e.grade from student s inner join enrollment e on s.roll_no=e.roll_no;
  select d.dep_name,count(*) as student_count from student s inner join department d on s.dep_id=d.dep_id group by d.dep_name;
+select d.dep_name,avg(s.cgpa) as avg_cgpa from student s inner join department d on s.dep_id=d.dep_id group by d.dep_name;
